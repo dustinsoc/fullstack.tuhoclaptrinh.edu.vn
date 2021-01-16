a5a086a9454a0ce8038ddc72c52684aeb71d4c19
dustinsoc <46445262+dustinsoc@users.noreply.github.com>
2021-01-16 16:45:57 +0700

    Update 002-sendJsonMessage.php

--- facebook-bot/002-sendJsonMessage.php
+++ facebook-bot/002-sendJsonMessage.php
@@ -1,43 +1,43 @@
 <?
-//! 1. Khai báo biến
-$token = ''; // Chuỗi access_token lấy được từ Facebook
-// Chuỗi JSON sử dụng template, xem thêm tại https://developers.facebook.com/docs/messenger-platform/send-messages/templates
-$json = '{
-  "recipient": {
-    "thread_key": "3196563110445368"
-  },
-  "message": {
-    "attachment": {
-      "type": "template",
-      "payload": {
-        "template_type": "button",
-        "text": "8h30 rồi kìa mấy má, vào System log giờ lẹ.",
-        "buttons": [
-          {
-            "type": "web_url",
-            "url": "https://system.socjsc.com/vi/tasks/",
-            "title": "Vào Sysem"
-          }
-        ]
+  //! 1. Khai báo biến
+  $token = ''; // Chuỗi access_token lấy được từ Facebook
+  // Chuỗi JSON sử dụng template, xem thêm tại https://developers.facebook.com/docs/messenger-platform/send-messages/templates
+  $json = '{
+    "recipient": {
+      "thread_key": "3196563110445368"
+    },
+    "message": {
+      "attachment": {
+        "type": "template",
+        "payload": {
+          "template_type": "button",
+          "text": "8h30 rồi kìa mấy má, vào System log giờ lẹ.",
+          "buttons": [
+            {
+              "type": "web_url",
+              "url": "https://system.socjsc.com/vi/tasks/",
+              "title": "Vào Sysem"
+            }
+          ]
+        }
       }
     }
-  }
-}';
+  }';
 
-//! 2. Khai báo thư viện và cấu hình
-new fb(); // Thư viện
-$soc->fb->setToken($token); // Cài đặt chuỗi token
+  //! 2. Khai báo thư viện và cấu hình
+  new fb(); // Thư viện
+  $soc->fb->setToken($token); // Cài đặt chuỗi token
 
-//! 3. Gửi và in ra mảng dữ liệu trả về
-$rs = $soc->fb->sendJsonMessage(json_decode($json, true));
-echo '<pre>';print_r($rs);exit; // Xem mảng dữ liệu trả về
+  //! 3. Gửi và in ra mảng dữ liệu trả về
+  $rs = $soc->fb->sendJsonMessage(json_decode($json, true));
+  echo '<pre>';print_r($rs);exit; // Xem mảng dữ liệu trả về
 
-//! 4. Mẫu kết quả
-/**********
-Array
-(
-    [message_id] => m_kj7memxJqCPAfG639AETCf5l0m2M9ETyitTLQDPXTxJFe1T_uyDMAjOpxm6PW_sqVLv9yf9OJhG7965tTOwVwg
-    [thread_key] => t_3196563110445368
-)
-***********/
+  //! 4. Mẫu kết quả
+  /**********
+  Array
+  (
+      [message_id] => m_kj7memxJqCPAfG639AETCf5l0m2M9ETyitTLQDPXTxJFe1T_uyDMAjOpxm6PW_sqVLv9yf9OJhG7965tTOwVwg
+      [thread_key] => t_3196563110445368
+  )
+  ***********/
 ?>
