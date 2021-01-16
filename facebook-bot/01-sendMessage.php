bebf306d22070e65ec2e9208698dd487fa3ebdd8
dustinsoc <46445262+dustinsoc@users.noreply.github.com>
2021-01-16 16:12:44 +0700

    Update 01-sendMessage.php

--- facebook-bot/01-sendMessage.php
+++ facebook-bot/01-sendMessage.php
@@ -1,1 +1,14 @@
+<?
+  //! 1. Khai báo biến
+  $token = ''; // Chuỗi access_token lấy được từ Facebook
+  $thread = ''; // ID của nhóm chat
+  $message = ''; // Nội dung tin nhắn, cho phép định dạng bằng markdown
 
+  //! 2. Khai báo thư viện và cấu hình
+  new fb(); // Thư viện
+  $soc->fb->setToken($token); // Cài đặt chuỗi token
+
+  //! 3. Gửi và in ra mảng dữ liệu trả về
+  $rs = $soc->fb->sendMessage($thread, $message); // Gửi
+  var_dump($rs); // In mảng kết quả
+?>
